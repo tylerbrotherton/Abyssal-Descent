dad64c0beb3e898b68b279afcfa824799196a152
Tyler Brotherton <105938811+tylerbrotherton@users.noreply.github.com>
2025-09-30 00:07:18 -0500

    Update ADResources.java
    I maked a stupid mistake and added a space in the parameters. I'm a dumbass. Forgive my last commit please

--- src/adresources/src/main/java/sh/slb/adresources/ADResources.java
+++ src/adresources/src/main/java/sh/slb/adresources/ADResources.java
@@ -1,7 +1,11 @@
 package sh.slb.adresources;
 
 import net.minecraftforge.fml.common.Mod;
-import sh.slb.adresources.SemaphoreBlock;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.IEventBus;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.BlockItem;
@@ -9,60 +13,61 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 @Mod(ADResources.MODID)
 public class ADResources {
-   public static final String MODID = "adresources";
+    public static final String MODID = "adresources";
 
-   public static DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-   public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Item> ITEMS  = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-   public ADResources() {
-      IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public ADResources() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		// TODO: wtf clean this up
-      register_block("bedrock1", 2);
-      register_block("bedrock2", 3);
-      register_block("bedrock3", 4);
-      register_block("bedrock4", 5);
-      register_block("bedrock5", 6);
+        // Register special Semaphore blocks (with levels)
+        registerSemaphoreBlock("bedrock1", 2);
+        registerSemaphoreBlock("bedrock2", 3);
+        registerSemaphoreBlock("bedrock3", 4);
+        registerSemaphoreBlock("bedrock4", 5);
+        registerSemaphoreBlock("bedrock5", 6);
 
-		register_non_semaphore_block("ore0");
-		register_non_semaphore_block("ore1");
-		register_non_semaphore_block("ore2");
-		register_non_semaphore_block("ore3");
-		register_non_semaphore_block("ore4");
-		register_non_semaphore_block("ore5");
-		register_non_semaphore_block("ore6");
-		register_non_semaphore_block("ore7");
-		register_non_semaphore_block("ore8");
-		register_non_semaphore_block("ore9");
+        // Register normal ore blocks
+        registerBasicBlock("ore0");
+		registerBasicBlock("ore1");
+		registerBasicBlock("ore2");
+		registerBasicBlock("ore3");
+		registerBasicBlock("ore4");
+		registerBasicBlock("ore5");
+		registerBasicBlock("ore6");
+		registerBasicBlock("ore7");
+		registerBasicBlock("ore8");
+		registerBasicBlock("ore9");		
 
-      BLOCKS.register(modEventBus);
-      ITEMS.register(modEventBus);
-   }
 
-	// TODO: prob invert naming cause sanity
-	private static void register_non_semaphore_block(String name) {
-		var block = BLOCKS.register(name, () -> new Block(
-			BlockBehaviour.Properties.of()
-				.mapColor(MapColor.STONE)
-				// .strength(50F, 36000F) // FIXME: idk what tf u want @eclipse
-		));
-		ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
-	}
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+    }
 
-   private static void register_block(String name, int level) {
-      var block = BLOCKS.register(name, () -> new SemaphoreBlock(
-         BlockBehaviour.Properties.of()
-            .mapColor(MapColor.STONE)
-            .strength(50F, 36000F), level
-      ));
-      ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
-   }
+    /** Registers a plain block + its BlockItem */
+    private static RegistryObject<Block> registerBasicBlock(String name) {
+        RegistryObject<Block> block = BLOCKS.register(name, 
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE))
+        );
+        ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        return block;
+    }
+
+    /** Registers a SemaphoreBlock with a level + its BlockItem */
+    private static RegistryObject<Block> registerSemaphoreBlock(String name, int level) {
+        RegistryObject<Block> block = BLOCKS.register(name, 
+            () -> new SemaphoreBlock(
+                BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.STONE)
+                    .strength(50F, 36000F), // why not?
+                level
+            )
+        );
+        ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        return block;
+    }
 }
